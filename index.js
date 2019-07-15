7fcad32b654e310e2fd5521addb3ae0180c3aeec
Jaimen Lathia <JaimenLathia@gmail.com>
2019-07-15 19:51:28 +0100

    feat: added second "cloud" api gateway

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ const awsx = require("@pulumi/awsx");
 const helloWorldHandler = () => async (event) => {
     return {
         statusCode: 200,
-        body: JSON.stringify({hello: "world"}),
+        body: JSON.stringify({hello: "from aws api"}),
     };
 };
 
-let endpoint = new awsx.apigateway.API("hello-world", {
+let endpoint = new awsx.apigateway.API("aws-api", {
     routes: [{
         path: "/",
         method: "GET",
@@ -20,4 +20,17 @@ let endpoint = new awsx.apigateway.API("hello-world", {
     }],
 });
 
-exports.endpoint = endpoint.url;
+exports.aws_url = endpoint.url;
+
+
+
+
+
+const cloud = require("@pulumi/cloud");
+const api = new cloud.API("cloud-api");
+
+api.get("/hello", (request, result) => {
+    result.json({ hello: "from cloud api" });
+});
+
+exports.cloud_url = api.publish().url;
