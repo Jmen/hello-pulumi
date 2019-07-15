905186f36e558101cfa387e4d3378695cbfe2d44
Jaimen Lathia <JaimenLathia@gmail.com>
2019-07-15 19:23:44 +0100

    feat: add lambda to root and "any" path of api gateway

--- index.js
+++ index.js
@@ -4,16 +4,22 @@
 const aws = require("@pulumi/aws");
 const awsx = require("@pulumi/awsx");
 
+const helloWorldHandler = () => async (event) => {
+    return {
+        statusCode: 200,
+        body: JSON.stringify({hello: "world"}),
+    };
+};
+
 let endpoint = new awsx.apigateway.API("hello-world", {
     routes: [{
         path: "/",
         method: "GET",
-        eventHandler: async (event) => {
-            return {
-                statusCode: 200,
-                body: JSON.stringify({ hello: "world" }),
-            };
-        },
+        eventHandler: helloWorldHandler(),
+    },{
+        path: "/{route+}",
+        method: "GET",
+        eventHandler: helloWorldHandler(),
     }],
 });
 
