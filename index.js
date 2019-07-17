c0b2dd4ee6df7fe588f98ab38939e435c424b196
Jaimen Lathia <JaimenLathia@gmail.com>
2019-07-17 19:04:00 +0100

    feat: removing cloud api

--- index.js
+++ index.js
@@ -31,16 +31,3 @@ for (const region of regions) {
     exports["aws_url_" + region] = endpoint.url;
 
 }
-
-const cloud = require("@pulumi/cloud");
-const api = new cloud.API("cloud-api");
-
-api.get("/", (request, result) => {
-    result.json({ hello: "from cloud api" });
-});
-
-api.get("/{route+}", (request, result) => {
-    result.json({ hello: "from cloud api" });
-});
-
-exports.cloud_url = api.publish().url;
